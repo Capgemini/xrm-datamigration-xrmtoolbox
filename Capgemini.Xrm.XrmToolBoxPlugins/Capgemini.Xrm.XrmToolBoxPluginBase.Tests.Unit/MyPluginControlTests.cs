fb1b6af76e0ad89eb0a1eae2bec6079525f39046
Ekhor <ekhor.asemota@capgemini.com>
2021-01-13 04:50:11 +0000

    started unit tests of myplugincontrol

---
+++ Capgemini.Xrm.XrmToolBoxPlugins/Capgemini.Xrm.XrmToolBoxPluginBase.Tests.Unit/MyPluginControlTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using McTools.Xrm.Connection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Moq;
+
+namespace MyXrmToolBoxPlugin3.Tests
+{
+    [TestClass]
+    public class MyPluginControlTests
+    {
+        [TestMethod]
+        public void MyPluginControl()
+        {
+            FluentActions.Invoking(() => new MyPluginControl())
+                         .Should()
+                         .NotThrow();
+        }
+
+        [TestMethod]
+        public void UpdateConnectionNullConnectionDetail()
+        {
+            var organisationService = new Mock<IOrganizationService>().Object;
+            ConnectionDetail detail = null;
+            string actionName = "Custom";
+            object parameter = null;
+
+            using (var systemUnderTest = new MyPluginControl())
+            {
+                FluentActions.Invoking(() => systemUnderTest.UpdateConnection(organisationService, detail, actionName, parameter))
+                             .Should()
+                             .NotThrow();
+            }
+        }
+    }
+}
