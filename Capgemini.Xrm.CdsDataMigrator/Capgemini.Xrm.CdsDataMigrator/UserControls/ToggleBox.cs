65d2ae7b5c652ff6a5482f8989d6379e0e1f418a
Ekhor <ekhor.asemota@capgemini.com>
2021-03-05 06:58:37 +0000

    fix: address sonar analysis bug

--- Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigrator/UserControls/ToggleBox.cs
+++ Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigrator/UserControls/ToggleBox.cs
@@ -9,6 +9,7 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBoxPlugin.UserControls
     using System.Drawing;
     using System.Drawing.Drawing2D;
     using System.Windows.Forms;
+
     public class ToggleCheckBox : CheckBox
     {
         public ToggleCheckBox()
@@ -16,6 +17,7 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBoxPlugin.UserControls
             SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
             Padding = new Padding(4);
         }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             this.OnPaintBackground(e);
@@ -27,7 +29,7 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBoxPlugin.UserControls
                 path.AddArc(d, d, r, r, 90, 180);
                 path.AddArc(this.Width - r - d, d, r, r, -90, 180);
                 path.CloseFigure();
-                e.Graphics.FillPath(Checked ? Brushes.LightGray : Brushes.LightGray, path);
+                e.Graphics.FillPath(Brushes.LightGray, path);
                 r = Height - 1;
                 var rect = Checked ? new Rectangle(Width - r - 1, 0, r, r)
                                    : new Rectangle(0, 0, r, r);
