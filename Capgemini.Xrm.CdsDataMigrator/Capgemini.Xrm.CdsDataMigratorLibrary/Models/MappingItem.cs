1c776c3ed4aa2f4b9ca3e374e4a0f4cdff8ec3fa
Daniel Patrick Roche <danielroche10@gmail.com>
2022-08-09 17:35:19 +0100

    Fix: removed unneccessary MappingItem.cs

--- Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigratorLibrary/Models/MappingItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Capgemini.Xrm.CdsDataMigratorLibrary.Models
-{
-    public class MappingItem
-    {
-        public string SourceId {  get; set; }
-        public string TargetId { get; set; }
-        public string Entity {  get; set; }
-    }
-}
