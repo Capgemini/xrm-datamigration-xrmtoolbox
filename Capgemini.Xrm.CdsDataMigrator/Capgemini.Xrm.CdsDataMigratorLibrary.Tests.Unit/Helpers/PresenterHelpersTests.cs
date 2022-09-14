b2874dbc258d8cff41d06d200166e00a378e9814
danroche10 <danielroche10@gmail.com>
2022-09-14 17:18:09 +0100

    Tests: testing second method in PresenterHelpers

--- Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigratorLibrary.Tests.Unit/Helpers/PresenterHelpersTests.cs
+++ Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigratorLibrary.Tests.Unit/Helpers/PresenterHelpersTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 using Capgemini.Xrm.CdsDataMigratorLibrary.Helpers;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,7 +10,7 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBox.Helpers.Tests
     public class PresenterHelpersTests
     {
         [TestMethod]
-        public void RowWithoutEmptyCellShouldReturnTrue()
+        public void AreAllCellsPopulated_RowWithoutEmptyCellShouldReturnTrue()
         {
             var rowWithBlankCell = GetRowWithoutBlankCell();
             var allCellsArePopulated = PresenterHelpers.AreAllCellsPopulated(rowWithBlankCell);
@@ -19,21 +20,36 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBox.Helpers.Tests
 
 
         [TestMethod]
-        public void RowWithEmptyCellShouldReturnFalse()
+        public void AreAllCellsPopulated_RowWithEmptyCellShouldReturnFalse()
         {
             var rowWithBlankCell = GetRowWithBlankCell();
             var allCellsArePopulated = PresenterHelpers.AreAllCellsPopulated(rowWithBlankCell);
 
             Assert.AreEqual(allCellsArePopulated, false);
         }
-        
-        private static DataGridViewRow GetRowWithBlankCell()
+
+        [TestMethod]
+        public void GetMappingsFromViewWithEmptyRowsRemoved_EmptyRowsShouldBeCorrectlyRemoved()
         {
-            DataGridViewRow dataGridViewRow = new DataGridViewRow();
-            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "Account" });
-            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "" });
-            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "accountid" });
-            return dataGridViewRow;
+            var lookUpMappings = new List<DataGridViewRow>();
+            var rowWithoutBlankCell = GetRowWithoutBlankCell();
+            var rowWithBlankCell = GetRowWithBlankCell();
+            lookUpMappings.Add(rowWithoutBlankCell);
+            lookUpMappings.Add(rowWithBlankCell);
+            var updatedLookupMappings = PresenterHelpers.GetMappingsFromViewWithEmptyRowsRemoved(lookUpMappings);
+            Assert.AreEqual(updatedLookupMappings.Count, 1);  
+        }
+
+        [TestMethod]
+        public void GetMappingsFromViewWithEmptyRowsRemoved_NoRowsShouldBeRemoved()
+        {
+            var lookUpMappings = new List<DataGridViewRow>();
+            var rowWithoutBlankCell = GetRowWithoutBlankCell();
+            var anotherRowWithoutBlankCell = GetRowWithoutBlankCell();
+            lookUpMappings.Add(rowWithoutBlankCell);
+            lookUpMappings.Add(anotherRowWithoutBlankCell);
+            var updatedLookupMappings = PresenterHelpers.GetMappingsFromViewWithEmptyRowsRemoved(lookUpMappings);
+            Assert.AreEqual(updatedLookupMappings.Count, 2);
         }
 
         private static DataGridViewRow GetRowWithoutBlankCell()
@@ -44,6 +60,17 @@ namespace Capgemini.Xrm.DataMigration.XrmToolBox.Helpers.Tests
             dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "accountid" });
             return dataGridViewRow;
         }
+
+        private static DataGridViewRow GetRowWithBlankCell()
+        {
+            DataGridViewRow dataGridViewRow = new DataGridViewRow();
+            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "Account" });
+            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "" });
+            dataGridViewRow.Cells.Add(new DataGridViewTextBoxCell { Value = "accountid" });
+            return dataGridViewRow;
+        }
+
+
 
     }
 }
