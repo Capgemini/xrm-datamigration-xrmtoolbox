caa63266951ecd0012df464e2226cb0e3f628cd3
Tom Ashworth <tom.ashworth@capgemini.com>
2022-11-07 09:27:17 +0000

    ci: move assembly attributes to `assemblyinfo.cs`

--- Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigrator/Properties/AssemblyInfo.cs
+++ Capgemini.Xrm.CdsDataMigrator/Capgemini.Xrm.CdsDataMigrator/Properties/AssemblyInfo.cs
@@ -8,3 +8,14 @@ using System.Reflection;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("94cfa298-b643-477b-a329-f184ebf68cf6")]
+
+[assembly: AssemblyTitle("Capgemini.Xrm.CdsDataMigrator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Capgemini")]
+[assembly: AssemblyProduct("Capgemini.Xrm.CdsDataMigrator")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
